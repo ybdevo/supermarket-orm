4e34db5e53d4b975115448c7f846ed702da11c05
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-08-24 19:33:03 +0530

    feat(entity): implemented JPA annotations for hibernate mapping for orderDetail

--- src/main/java/lk/ijse/supermarketfx/entity/OrderDetail.java
+++ src/main/java/lk/ijse/supermarketfx/entity/OrderDetail.java
@@ -1,7 +1,9 @@
 package lk.ijse.supermarketfx.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -10,9 +12,19 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "order_details")
 public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String orderId;
-    private String itemId;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "item_code")
+    private Item item;
     private int quantity;
+    @Column(name = "unit_price", precision = 10, scale = 2)
     private BigDecimal price;
 }
